fe814a08c3163a5db1897932f60a8bc0cc654945
SergioLosBoscos <sergio.perez@salesianoslosboscos.com>
2023-09-11 10:14:05 +0200

    tarea1-titulo-1013
    Se ha añadido una fuete, el Titulo y un Gradiant para el fondo

---
+++ lib/Homepage.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.all(30),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          stops: [
+            0.1,
+            0.4,
+            0.6,
+            0.9,
+          ],
+          colors: [
+            Colors.yellow,
+            Colors.red,
+            Colors.indigo,
+            Colors.teal,
+          ],
+        )),
+        child: Center(
+          child: Column(
+
+            children: [
+
+              Container(
+                padding: EdgeInsets.only(top: 150),
+
+                child: Text('TRIVIAL',style: TextStyle(fontSize: 80),),
+              ),
+              ElevatedButton(onPressed: onPressed, child: Text('Jugar')),
+              DropdownMenu<String>(dropdownMenuEntries: dropdownMenuEntries)
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
